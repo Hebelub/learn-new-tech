2a5b00915a55be63e1f17d16a90e8a0c2fa37eb8
Gabriel Løsnesløkken <gabriell@uia.no>
2024-03-06 16:56:02 +0100

    Create template for TimeBooker component

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,9 @@
-import { Button } from "@/components/ui/button"
+import TimeBooker from "@/components/TimeBooker"
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-center p-24">
-      <Button>Click me</Button>
+      <TimeBooker />
     </main>
   );
 }
