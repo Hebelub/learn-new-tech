3c7f376f0660a9d118f82d457475f9b7280ebab7
Gabriel Løsnesløkken <gabriell@uia.no>
2024-03-06 22:29:52 +0100

    Add select date

--- components/TimeBooker.tsx
+++ components/TimeBooker.tsx
@@ -1,14 +1,24 @@
-import React from 'react'
+"use client"
+
+import React, { useState } from 'react'
 import { Button } from "@/components/ui/button"
 import { Calendar } from "@/components/ui/calendar"
 import { ScrollArea } from "@/components/ui/scroll-area"
 import { Skeleton } from "@/components/ui/skeleton"
 
 function TimeBooker() {
+
+    const [date, setDate] = useState<Date | undefined>(new Date())
+
     return (
         <div className="flex flex-col items-center">
             <div className="flex justify-center items-stretch space-x-4">
-                <Calendar />
+                <Calendar
+                    mode="single"
+                    selected={date}
+                    onSelect={setDate}
+                    className="rounded-md border"
+                />
                 <ScrollArea className="h-[350px] w-[350px] rounded-md border p-4 overflow-y-auto">
                     {Array.from({ length: 24 }, (_, index) => (
                         <Skeleton key={index} className="h-[40px] rounded-full my-4" />
