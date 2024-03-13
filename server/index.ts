2eb0c5b27d729d991354daea3d2602de5effe42c
Gabriel Løsnesløkken <gabriell@uia.no>
2024-03-13 15:20:35 +0100

    Create post todo function to db with zod validation

--- server/index.ts
+++ server/index.ts
@@ -1,6 +1,7 @@
 import { drizzle } from "drizzle-orm/better-sqlite3";
 import { migrate } from "drizzle-orm/better-sqlite3/migrator";
 import Database from "better-sqlite3";
+import { z } from "zod";
 
 import { publicProcedure, router } from "./trpc";
 
@@ -15,6 +16,10 @@ export const appRouter = router({
     getTodos: publicProcedure.query(async () => {
         return await db.select().from(todos).all();
     }),
+    addTodo: publicProcedure.input(z.string()).mutation(async (opts) =>  {
+        await db.insert(todos).values({ content: opts.input, done: 0 }).run();
+        return true;
+    })
 });
 
 export type AppRouter = typeof appRouter;
