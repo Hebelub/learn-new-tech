40feabad9b05b220c4b087ce933f9cc68b987960
Gabriel Løsnesløkken <gabriell@uia.no>
2024-03-13 15:29:26 +0100

    List todos and style them

--- app/_components/TodoList.tsx
+++ app/_components/TodoList.tsx
@@ -15,8 +15,20 @@ export default function TodoList() {
 
     return (
         <div>
-            <div>{JSON.stringify(getTodos.data)}</div>
-            <div>
+            <div className="text-black my-5 text-3xl">
+                {getTodos?.data?.map((todo) => (
+                    <div key={todo.id} className="flex gap-3 items-center">
+                        <input
+                            id={`check-${todo.id}`}
+                            type="checkbox"
+                            checked={!!todo.done}
+                            style={{ zoom: 1.5 }}
+                        />
+                        <label htmlFor={`check-${todo.id}`}>{todo.content}</label>
+                    </div>
+                ))}
+            </div>
+            <div className="flex gap-3 items-center">
                 <label htmlFor="content">Content</label>
                 <input
                     id="content"
